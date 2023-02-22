97644d6746ed8d85543ac09909d2a4694a310e16
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-02-22 18:34:53 +0500

    adding menu button for small screen in header

--- panaverse-website-chakra/src/app/Component/Footer.tsx
+++ panaverse-website-chakra/src/app/Component/Footer.tsx
@@ -12,19 +12,26 @@ export default function Footer() {
         <Box bg='black'>
             <Container maxW='1300px'>
 
-                <SimpleGrid py='60px' templateColumns='repeat(4, 1fr)'>
-                    {/*gridTemplateColumns is a CSS property that specifies the size of each column in a grid while templateColumns is a prop specific to SimpleGrid that allows
-                    you to specify the number of columns at different breakpoints */}
+                <SimpleGrid spacing='20px' gap='20px' py='60px' templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)' }} textAlign={{ base: 'center', lg: 'left' }} >
 
-                    <Box color='gray'>
-                        <Heading pb='15px' color='white'>About Us</Heading>
-                        <Image src={png} alt='Panaverse' />
-                        <Text pt='15px' pr='50' >The Future of the Web is Web3.0, Metaverse, and Edge Computing. Panaverse DAO is a movement to spread these technologies globally. It
+                    {/*gridTemplateColumns is a CSS property that specifies the size of each column in a grid while templateColumns is a prop specific to SimpleGrid that allows
+                    you to specify the number of columns at different breakpoints
+                    If mobile screen, display 2 columns and 4 columns if laptop screen
+                    All the items will be center aligned if small screen and left align if large screen */}
+
+                    <Box color='gray' >
+
+                        <Heading pb='20px' color='white'>About Us</Heading>
+
+                        <Image style={{ margin: { base: 'auto', lg: '0px' } }} src={png} alt='Panaverse' />
+
+                        <Text pt='15px' pr={{ base: '0', lg: '50' }} >The Future of the Web is Web3.0, Metaverse, and Edge Computing. Panaverse DAO is a movement to spread these technologies globally. It
                             is community of Web3 abd Metaverse developers, designers, trainers, startup founders, and service providers.
                         </Text>
+
                     </Box>
 
-                    <Box>
+                    <Box >
 
                         <Heading color='white'>Useful Links</Heading>
 
@@ -43,7 +50,7 @@ export default function Footer() {
                         </Grid>
                     </Box>
 
-                    <Box>
+                    <Box >
                         <Heading color='white'>Follow Us</Heading>
 
                         <Grid pt='40px' color='gray'>
@@ -62,7 +69,7 @@ export default function Footer() {
 
                     </Box>
 
-                    <Box color='white'>
+                    <Box>
 
                         <Heading color='white'>Contact Us</Heading>
                         <Grid pt='40px' color='gray'>
--- panaverse-website-chakra/src/app/Component/Header.tsx
+++ panaverse-website-chakra/src/app/Component/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Box, Container, SimpleGrid, Flex, Button } from '@chakra-ui/react'
+import { Box, Container, SimpleGrid, Flex, Button, Menu, IconButton, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
 import Image from 'next/image'
 import png from '../../../public/panaverse80_80.png'
 import Link from 'next/link'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 export default function Header() {
     return (
@@ -12,7 +13,7 @@ export default function Header() {
             
             maxW specifies the maximum width of the container*/}
 
-                <SimpleGrid templateColumns='repeat(3,1fr)'> {/*a layout component in the Chakra UI React component library that provides a simple and easy-to-use grid system for 
+                <SimpleGrid templateColumns={{ sm: 'repeat(3,1fr)', lg: 'repeat(3,1fr)' }}> {/*a layout component in the Chakra UI React component library that provides a simple and easy-to-use grid system for 
                 building responsive web layouts
                 
                 templateColumns prop is used to define the columns of a grid layout. repeat function is used to define three columns with equal widths using 1 fraction (fr) unit
@@ -25,11 +26,12 @@ export default function Header() {
 
                     </Box>
 
-                    <Flex placeItems='center' gap={10} fontSize='18px' fontWeight='semibold'> {/*it is used for creating flexible layouts that allow the positioning and alignment of child elements in a container. Using it, you can create layouts that are both
+                    <Flex display={{ lg: 'flex', base: 'none' }} placeItems='center' gap={10} fontSize='18px' fontWeight='semibold'> {/*it is used for creating flexible layouts that allow the positioning and alignment of child elements in a container. Using it, you can create layouts that are both
                     flexible and responsive, allowing one to easily adjust the layout of the web pages to fit different screen sizes and devices. We will provide 5 links to 5 pages, 
                     so we will include 5 link components
                     
                     gap property places gap between the items in the flex
+                    display property is used to display items when large screen and to hide the flex content when screen shrinks
                     placeItems property places the items within the flew to the center*/}
 
                         <Link href=''>Home</Link>
@@ -44,12 +46,44 @@ export default function Header() {
 
                     </Flex>
 
-                    <Box > {/*Separate box for the apply button */}
+                    <Box display={{ base: 'none', lg: 'initial' }} pt='25px'> {/*Separate box for the apply button */}
+
                         <Button size='lg' colorScheme='teal' float='right' mt='10px'>Apply</Button> {/*The button will shift to the right side of the page
                         
                     colorScheme* is used to specify the color scheme for the button
                     size sets the size of button to lg
                     mt is used to add margin outside the element of 10px*/}
+                    </Box>
+
+                    <Box display={{ base: 'inital', lg: 'none' }}>
+
+                        <Menu >
+
+                            <MenuButton as={IconButton}
+                                float='left'
+                                aria-label='Options' //provides an accessible name for the menu and is visible when there is no visible label for the menu
+                                icon={<HamburgerIcon />} //icon to render before the menu item's label
+                                variant='menu' //allows to ustomise the appearance of the menu
+                            />
+
+                            <MenuList>
+
+                                <MenuItem>
+                                    <Link href='/'></Link>
+                                </MenuItem>
+
+                                <MenuItem>Syllabus</MenuItem>
+
+                                <MenuItem>Explore</MenuItem>
+
+                                <MenuItem>About</MenuItem>
+
+                                <MenuItem>Contact</MenuItem>
+
+                            </MenuList>
+
+                        </Menu>
+
                     </Box>
 
                 </SimpleGrid>
