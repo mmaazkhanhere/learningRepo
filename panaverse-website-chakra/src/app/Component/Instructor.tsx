af4f21eb60e0be8efc4447d9e3094b543fafa408
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-02-23 22:48:42 +0500

    creating instructor list using arr.map

--- panaverse-website-chakra/src/app/Component/Instructor.tsx
+++ panaverse-website-chakra/src/app/Component/Instructor.tsx
@@ -1,5 +1,14 @@
+/* eslint-disable react/jsx-key */
 import React from 'react'
 import { Box, Container, Heading, Text, Flex, Center, SimpleGrid, Image } from '@chakra-ui/react'
+import { data } from './database'
+
+interface data {
+    id: number,
+    src: string,
+    heading: string,
+    text: string
+}
 
 export default function Instructor() {
     return (
@@ -12,93 +21,28 @@ export default function Instructor() {
                     <Heading pb='25px'>Our Instructors</Heading>
                 </Center>
 
-                <SimpleGrid minChildWidth='200px' pb='20px'>
+                <Flex flexDir='row'>
 
-                    <Box >
-                        <Center>
+                    {data.map((ins: data) => /*map function is used on array of object that is recieved from the database. It is used to tidy our code as copying and pasting boxes 
+                   will cluter the code and also increase its size*/
+                        <Box >
+                            <Center>
 
-                            <Image borderRadius='full' src='/zia.jpg' alt='Zia Khan' height='100px' width='100px' />{/*Chakra Image Component not NextJs Image component
-                            borderRadius 'full' will make round image. */}
+                                <Image borderRadius='full' src={ins.src} alt='Zia Khan' height='90px' width='90px' />{/*Chakra Image Component not NextJs Image component
+                            borderRadius 'full' will make round image.
+                            src component is recieved from the database file */}
 
-                        </Center>
+                            </Center>
 
-                        <Heading pt='10px' textAlign='center'>Zia Khan</Heading>
+                            <Heading pt='10px' textAlign='center' size='md'>{ins.heading}</Heading> {/*heading is recieved from the database file */}
 
-                        <Text pt='5px' textAlign='center' px='20px'>
-                            Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project
-                            management, and API and App development
-                        </Text>
+                            <Text size='sm' pt='5px' textAlign='center' px='20px'>
+                                {ins.text}
+                            </Text>
 
-                    </Box>
-
-                    <Box >
-                        <Center>
-
-                            <Image borderRadius='full' src='/zia.jpg' alt='Zia Khan' height='100px' width='100x' />{/*Chakra Image Component not NextJs Image component
-                            borderRadius 'full' will make round image. */}
-
-                        </Center>
-
-                        <Heading pt='10px' textAlign=' center'>Zia Khan</Heading>
-
-                        <Text pt='5px' textAlign='center' px='20px'>
-                            Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project
-                            management, and API and App development
-                        </Text>
-
-                    </Box>
-
-                    <Box>
-                        <Center>
-
-                            <Image borderRadius='full' src='/zia.jpg' alt='Zia Khan' height='100px' width='100px' />{/*Chakra Image Component not NextJs Image component
-                            borderRadius 'full' will make round image. */}
-
-                        </Center>
-
-                        <Heading pt='10px' textAlign='center'>Zia Khan</Heading>
-
-                        <Text pt='5px' textAlign='center' px='20px'>
-                            Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project
-                            management, and API and App development
-                        </Text>
-
-                    </Box>
-
-                    <Box>
-                        <Center>
-
-                            <Image borderRadius='full' src='/zia.jpg' alt='Zia Khan' height='100px' width='100px' />{/*Chakra Image Component not NextJs Image component
-                            borderRadius 'full' will make round image. */}
-
-                        </Center>
-
-                        <Heading pt='10px' textAlign='center'>Zia Khan</Heading>
-
-                        <Text pt='5px' textAlign='center' px='20px'>
-                            Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project
-                            management, and API and App development
-                        </Text>
-
-                    </Box >
-
-                    <Box>
-                        <Center>
-
-                            <Image borderRadius='full' src='/zia.jpg' alt='Zia Khan' height='100px' width='100px' />{/*Chakra Image Component not NextJs Image component
-                            borderRadius 'full' will make round image. */}
-
-                        </Center>
-
-                        <Heading pt='10px' textAlign='center'>Zia Khan</Heading>
-
-                        <Text pt='5px' textAlign=' center' px='20px'>
-                            Mentor and software developer with 20 + years of expertise in cloud and serverless computing, software design, project
-                            management, and API and App development
-                        </Text >
-
-                    </Box >
-                </SimpleGrid >
+                        </Box>
+                    )}
+                </Flex >
             </Container >
         </Box >
     )
---
+++ panaverse-website-chakra/src/app/Component/database.tsx
@@ -0,0 +1,33 @@
+export const data = [ /*any array of object for the instructors that will be recieved in the instructor component. It will be used by a map function that will generate an array 
+of instructor and display it. The map function will use the id property of the object to map over all the objects*/
+    {
+        id: 1,
+        src: '/zia.jpg',
+        heading: 'Zia Khan',
+        text: 'Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project management, and API and App development'
+    },
+    {
+        id: 2,
+        src: '/zia.jpg',
+        heading: 'Zeeshan Hanif',
+        text: 'Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project management, and API and App development'
+    },
+    {
+        id: 3,
+        src: '/zia.jpg',
+        heading: 'Adil Altaf',
+        text: 'Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project management, and API and App development'
+    },
+    {
+        id: 4,
+        src: '/zia.jpg',
+        heading: 'Hira Khan',
+        text: 'Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project management, and API and App development'
+    },
+    {
+        id: 5,
+        src: '/zia.jpg',
+        heading: 'Daniyal Nagori',
+        text: 'Mentor and software developer with 20+ years of expertise in cloud and serverless computing, software design, project management, and API and App development'
+    }
+]
