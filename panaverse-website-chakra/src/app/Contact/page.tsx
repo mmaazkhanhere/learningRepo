684596af56d187ea3c98b8690692bcc164fcbe59
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-02-23 19:13:19 +0500

    adding content to the about page

--- panaverse-website-chakra/src/app/Contact/page.tsx
+++ panaverse-website-chakra/src/app/Contact/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import React from 'react'
+import Home from '../Component/Home'
+import { Box, Heading } from '@chakra-ui/react'
 
 export default function Contact_Page() {
     return (
-        <div>Contact_Page</div>
+        <Home title='Contact Us' src='https://cdni.iconscout.com/illustration/premium/thumb/contact-us-3483601-2912018.png' />
     )
 }
--- panaverse-website-chakra/src/app/Syllabus/page.tsx
+++ panaverse-website-chakra/src/app/Syllabus/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import React from 'react'
+import Home from '../Component/Home'
+import { Heading } from '@chakra-ui/react'
 
 export default function Syllabus_page() {
     return (
-        <div>Syllabus Page</div>
+        <Home title='Syllabus' src='https://leverageedublog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2019/11/23173153/Current-Education-System-in-India.png' />
     )
 }
