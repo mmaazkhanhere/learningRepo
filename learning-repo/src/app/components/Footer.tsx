9fc092ee2b6316cde991ade961e999ab2e5d8778
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-03-28 21:40:08 +0500

    footer, header created

---
+++ learning-repo/src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='flex justify-center items-center h-10 shadow-inner'>
+            Footer
+        </div>
+    )
+}
