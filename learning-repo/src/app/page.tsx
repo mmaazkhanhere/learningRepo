7d55952ec2df0fd7afc998765578f6f1c1c773e7
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-03-28 23:14:23 +0500

    product created

--- learning-repo/src/app/page.tsx
+++ learning-repo/src/app/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import data from '../../utils/data'
+import ProductItem from './components/ProductItem'
 
 export default function Hello() {
   return (
     <div className='container m-auto mt-4 px-4'>
-      Home Page
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
     </div>
   )
 }
