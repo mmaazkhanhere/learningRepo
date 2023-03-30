a70863fec3b90123f38693528bd2da426a25afbf
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-03-30 14:52:33 +0500

    starting new project for ecommerce website

--- learning-repo/src/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <div className='flex justify-center items-center h-10 shadow-inner'>
-            Footer
-        </div>
-    )
-}
