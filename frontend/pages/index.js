bbccdd03ec636d5e267442e6df1863da90f1b909
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-04-05 12:36:02 +0500

    heroBanner section completed

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,9 @@
+import HeroBanner from "@/components/HeroBanner";
 
 export default function Home() {
   return (
-    <main className="h-[2000px]">
-      Home Page...
+    <main>
+      <HeroBanner/>
     </main>
   )
 }
