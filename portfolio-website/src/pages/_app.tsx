f133d2586dddfa7020c40f4a6ccd3738627f6b05
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-05-03 15:29:25 +0500

    navigation bar created

--- portfolio-website/src/pages/_app.tsx
+++ portfolio-website/src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import NavBar from './components/NavBar'
 
 export default function App({ Component, pageProps }: AppProps) {
   <Head>
     <title>Portfolio Website</title>
   </Head>
-  return <Component {...pageProps} />
-
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
