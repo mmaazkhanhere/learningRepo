2fe29319f7c75f71be54f39dec57a0e0b6f142c9
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-05-04 16:14:58 +0500

    created hero banner

--- portfolio-website/src/pages/index.tsx
+++ portfolio-website/src/pages/index.tsx
@@ -18,6 +18,13 @@ export default function Home() {
             <div className='w-1/2'>
               <Image src={profile} alt='Profile Picture' className='w-full h-auto' />
             </div>
+            <div className='w-1/2'>
+              <h1>Turning Vision Into Reality With Code And Design</h1>
+              <p>
+                As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+                Explore my latest projects and articles, showcasing my expertise in React.js and web development
+              </p>
+            </div>
           </div>
         </Layout>
       </main>
