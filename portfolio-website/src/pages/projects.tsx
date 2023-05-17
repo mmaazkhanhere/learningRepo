2a670b5a04d05e27c41db3aa3380a70dbb709d2d
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-05-17 17:39:55 +0500

    dark theme setting for project page

--- portfolio-website/src/pages/projects.tsx
+++ portfolio-website/src/pages/projects.tsx
@@ -12,28 +12,34 @@ const FramerImage = motion(Image)
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
     return (
         <article className='w-full flex items-center relative justify-between rounded-3xl border border-solid
-         border-dark bg-light shadow-2xl p-12 rounded-br-2xl'>
+         border-dark bg-light shadow-2xl p-12 rounded-br-2xl dark:bg-dark dark:border-light'
+        >
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
-            rounded-br-3xl' />
+            rounded-br-3xl dark:bg-light'
+            />
             <Link href={link} target="_blank" className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
                 <FramerImage src={img} alt={title} className="w-full h-auto"
                     whileHover={{ scale: 1.05 }}
                     transition={{ duration: 0.2 }} />
             </Link>
             <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-                <span className='text-primary font-medium text-xl'>{type}</span>
+                <span className='text-primary font-medium text-xl dark:text-primaryDark'>
+                    {type}
+                </span>
                 <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-                    <h2 className='my-2 w-full text-left text-4xl font-bold '>
+                    <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light'>
                         {title}
                     </h2>
                 </Link>
-                <p className='my-2 font-medium text-dark'>{summary}</p>
+                <p className='my-2 font-medium text-dark dark:text-light'>
+                    {summary}
+                </p>
                 <div className='mt-2 flex items-center'>
                     <Link href={github} target="_blank" className='w-10'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 1024 1024"><path fill="currentColor" d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5C64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9c26.4 39.1 77.9 32.5 104 26c5.7-23.5 17.9-44.5 34.7-60.8c-140.6-25.2-199.2-111-199.2-213c0-49.5 16.3-95 48.3-131.7c-20.4-60.5 1.9-112.3 4.9-120c58.1-5.2 118.5 41.6 123.2 45.3c33-8.9 70.7-13.6 112.9-13.6c42.4 0 80.2 4.9 113.5 13.9c11.3-8.6 67.3-48.8 121.3-43.9c2.9 7.7 24.7 58.3 5.5 118c32.4 36.8 48.9 82.7 48.9 132.3c0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9c177.1-59.7 304.6-227 304.6-424.1c0-247.2-200.4-447.3-447.5-447.3z" /></svg>
                     </Link>
                     <Link href={link} target="_blank" className='ml-4 rounded-lg bg-dark text-light p-2 px-6
-                    text-lg font-semibold'>
+                    text-lg font-semibold dark:bg-light dark:text-dark'>
                         Visit Project
                     </Link>
                 </div>
@@ -45,9 +51,9 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 const Project = ({ title, type, img, link, github }) => {
     return (
         <article className='w-full flex flex-col items-center justify-center rounded-2xl border border-solid 
-        border-dark bg-light relative p-6 '>
+        border-dark bg-light relative p-6 dark:bg-dark dark:border-light'>
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
-            rounded-br-3xl' />
+            rounded-br-3xl dark:bg-light' />
             <Link href={link} target="_blank" className='w-full cursor-pointer overflow-hidden 
             rounded-lg'>
                 <FramerImage src={img} alt={title} className="w-full h-auto"
@@ -55,7 +61,7 @@ const Project = ({ title, type, img, link, github }) => {
                     transition={{ duration: 0.2 }} />
             </Link>
             <div className='w-full flex flex-col items-start justify-between mt-4'>
-                <span className='text-primary font-medium text-xl'>{type}</span>
+                <span className='text-primary font-medium text-xl dark:text-primaryDark'>{type}</span>
                 <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
                     <h2 className='my-2 w-full text-left text-3xl font-bold '>
                         {title}
@@ -82,7 +88,7 @@ export default function Projects() {
                 <title>Maaz Khan | Projects Page</title>
                 <meta name='description' content='created with nextjs' />
             </Head>
-            <main className='w-full mb-16 flex flex-col items-center justify-center'>
+            <main className='w-full mb-16 flex flex-col items-center justify-center dark:text-light'>
                 <Layout className='pt-16'>
                     <AnimatedText text="Imagination Trumps Knowledge!" className='mb-16' />
                     <div className='grid grid-cols-12 gap-24 gap-y-32'>
