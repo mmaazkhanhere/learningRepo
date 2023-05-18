68ab29c06baf85095dd9a8021e65a44500e1d590
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-05-18 17:30:07 +0500

    biography made responsive

--- portfolio-website/src/pages/about.tsx
+++ portfolio-website/src/pages/about.tsx
@@ -43,10 +43,12 @@ export default function about() {
             </Head>
             <main className='flex w-full flex-col items-center justify-center dark:text-light'>
                 <Layout className='pt-16'>
-                    <AnimatedText text="Passion Fuels Purposes !" className='mb-16' />
+                    <AnimatedText text="Passion Fuels Purposes !" className='mb lg:!text-7xl sm:!text-6xl
+                    xs:!text-4xl sm:mb-8' />
 
-                    <div className='grid w-full grid-cols-8 gap-16'>
-                        <div className='col-span-3 flex flex-col items-start justify-start'>
+                    <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                        <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4
+                        md:order-2 md:col-span-8'>
                             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75
                             dark:text-light/75'>
                                 Biography
@@ -68,7 +70,8 @@ export default function about() {
                         </div>
                         {/*Profile Image */}
                         <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid
-                        border-dark dark:bg-dark dark:border-light bg-light p-8'>
+                        border-dark dark:bg-dark dark:border-light bg-light p-8 xl:col-span-4 
+                        md:order-1 md:col-span-8'>
                             <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem]
                             bg-dark dark:bg-light'/>
                             <Image src={Profile} alt='Profile Picture'
@@ -81,31 +84,32 @@ export default function about() {
                         </div>
 
                         {/*Experiences */}
-                        <div className='col-span-2 flex flex-col items-end justify-between'>
-                            <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'>
+                        <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 
+                        xl:flex-row xl:items-center md:order-3'>
+                            <div className='flex flex-col items-end justify-center xl:items-center'>
+                                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumber value={50} />+
                                 </span>
                                 <h2 className='text-xl font-medium capitalize text-dark/75 
-                                dark:text-light'>
+                                dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm'>
                                     Satisfied Clients
                                 </h2>
                             </div>
                             <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'>
+                                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumber value={40} />+
                                 </span>
                                 <h2 className='text-xl font-medium capitalize text-dark/75
-                                dark:text-light'>
+                                dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm'>
                                     Projects Completed
                                 </h2>
                             </div>
                             <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'>
+                                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumber value={4} />+
                                 </span>
                                 <h2 className='text-xl font-medium capitalize text-dark/75
-                                dark:text-light'>
+                                dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm'>
                                     Experience
                                 </h2>
                             </div>
