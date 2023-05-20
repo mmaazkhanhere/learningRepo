08a34c1311844e2d112f09daf6ee7de0767531b9
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-05-20 16:12:10 +0500

    skills section responsive

--- portfolio-website/src/pages/about.tsx
+++ portfolio-website/src/pages/about.tsx
@@ -95,7 +95,7 @@ export default function about() {
                                     Satisfied Clients
                                 </h2>
                             </div>
-                            <div className='flex flex-col items-end justify-center'>
+                            <div className='flex flex-col items-end justify-center xl:items-center'>
                                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumber value={40} />+
                                 </span>
@@ -104,13 +104,13 @@ export default function about() {
                                     Projects Completed
                                 </h2>
                             </div>
-                            <div className='flex flex-col items-end justify-center'>
+                            <div className='flex flex-col items-end justify-center xl:items-center'>
                                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumber value={4} />+
                                 </span>
                                 <h2 className='text-xl font-medium capitalize text-dark/75
                                 dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                                    Experience
+                                    Years of Experience
                                 </h2>
                             </div>
                         </div>
