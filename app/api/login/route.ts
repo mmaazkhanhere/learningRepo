199a96ccc878b4c99ba2902362a7c8d55edef4be
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-08-09 15:28:05 +0500

    login functionality also completed

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -12,15 +12,48 @@ export const POST = async (request: NextRequest) => {
 
     try {
 
-        const username = request.cookies.get("username") ?? null;
-        const password = request.cookies.get("password") ?? null;
-        const token = request.cookies.get("token") ?? null;
+        const { value: username } = request.cookies.get("username") ?? { value: null };
+        const { value: password } = request.cookies.get("password") ?? { value: null };
+        const { value: token } = request.cookies.get("token") ?? { value: null };
+        const { value: address } = request.cookies.get("token") ?? { value: null };
 
-        // const tokenVerified=token && verifyJwtToken(token);
+        const tokenVerified = token && verifyJwtToken(token);
 
         console.log(username, password, token);
 
-        // if(username===signInUsername && password===singInPassword)
+        if (username === signInUsername && password === singInPassword) {
+
+            if (tokenVerified) {
+                // Token verified, user is already authenticated
+                const response = NextResponse.json(
+                    { success: true },
+                    { status: 200, headers: { "content-type": "application/json" } }
+                );
+                return response;
+            }
+
+            // Token not verified, generate and set a new token
+            const token = await new SignJWT({
+                address: address,
+                role: 'admin'
+            })
+                .setProtectedHeader({ alg: "HS256", typ: 'JWT' })
+                .setIssuedAt()
+                .setExpirationTime("300s")
+                .sign(getJwtSecretKey());
+
+            const response = NextResponse.json(
+                { success: true },
+                { status: 200, headers: { "content-type": "application/json" } }
+            );
+
+            response.cookies.set({
+                name: "token", value: token, path: "/"
+            });
+
+            return response;
+        }
+
 
     } catch (error) {
         console.error("Error creating token:", error);
