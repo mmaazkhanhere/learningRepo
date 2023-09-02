9bfed1c11623fb4785592095887f62fd1d2fb4c2
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-09-02 18:13:40 +0500

    Sidebar created

--- app/(root)/(routes)/layout.tsx
+++ app/(root)/(routes)/layout.tsx
@@ -1,10 +1,16 @@
-import Navbar from "@/components/navbar";
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+import Navbar from "@/components/navbar";
+import { Sidebar } from "@/components/sidebar";
+
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+
     return (
         <div className="h-full">
             <Navbar />
-            <main className="md:pl-20 pt-16 h-full" >
+            <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
+                <Sidebar />
+            </div>
+            <main className="md:pl-20 pt-16 h-full">
                 {children}
             </main>
         </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
+import MobileSidebar from "./mobile-sidebar";
 
 const font = Poppins({
     weight: "600",
@@ -17,9 +18,9 @@ const font = Poppins({
 const Navbar = () => {
     return (
         <div className="fixed w-full z-50 flex justify-between
-        items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
-                <Menu className="block md:hidden" />
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn(`hidden md:block text-xl md:text-3xl font-bold
                     text-primary`,
