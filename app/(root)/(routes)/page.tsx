98dfb5ccd80f7eb2146a8601de7566865f93e9e7
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-09-14 20:30:32 +0500

    companion list completed

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,15 +1,45 @@
 import { Categories } from '@/components/categories'
+import { Companions } from '@/components/companions'
 import SearchInput from '@/components/search-input'
 import prismadb from '@/lib/prismadb'
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
-const RootPage = async () => {
+interface RootPageProps {
+    searchParams: {
+        categoryId: string,
+        name: string;
+    }
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+
     const categories = await prismadb.category.findMany()
+
+    const data = await prismadb.companion.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: "desc"
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
+
     return (
         <div className='h-full p-4 space-y-2'>
             <SearchInput />
             <Categories data={categories} />
+            <Companions data={data} />
         </div>
     )
 }
