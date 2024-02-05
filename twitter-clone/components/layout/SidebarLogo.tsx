81e0cc21a1d578496b9712e45018ad3395de77f4
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-02-05 15:46:37 +0500

    layout ui completed

---
+++ twitter-clone/components/layout/SidebarLogo.tsx
@@ -0,0 +1,29 @@
+/*Logo component */
+
+import { useRouter } from "next/router";
+import { BsTwitter } from "react-icons/bs";
+
+const SidebarLogo = () => {
+    const router = useRouter();
+
+    return (
+        <div
+            onClick={() => router.push('/')} //redirects to home page
+            className="
+        rounded-full 
+        h-14
+        w-14
+        p-4 
+        flex 
+        items-center 
+        justify-center 
+        hover:bg-blue-300 
+        hover:bg-opacity-10 
+        cursor-pointer
+    ">
+            <BsTwitter size={28} color="white" /> {/*Icons */}
+        </div>
+    );
+};
+
+export default SidebarLogo;
