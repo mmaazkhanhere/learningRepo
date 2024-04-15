cc9a53e614dc345cd20569e6d1df0ae187d16bbc
Maaz Khan <mmaazkhan2107@gmail.com>
2024-04-15 13:29:05 +0500

    course creation ui completed

--- lms-tutorial/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ lms-tutorial/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,10 +1,23 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const Courses = (props: Props) => {
     return (
-        <div>Courses</div>
+        <div
+            className='p-6'
+        >
+            <Link
+                href='/teacher/create'
+            >
+                <Button>
+                    New Course
+                </Button>
+            </Link>
+
+        </div>
     )
 }
 
