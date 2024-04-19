753a480a248ac22a750fdabeaf3a926d27cbe3ad
Maaz Khan <mmaazkhan2107@gmail.com>
2024-04-19 16:22:43 +0500

    chapter edit page ui completed

--- lms-tutorial/app/(dashboard)/layout.tsx
+++ lms-tutorial/app/(dashboard)/layout.tsx
@@ -8,12 +8,17 @@ type Props = {
 const DashboardLayout = ({ children }: Props) => {
     return (
         <div className="h-full">
+            {/*Navbar that consists of user avatar and teacher button */}
             <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
                 <Navbar />
             </div>
+
+            {/*Sidebar that consists of navigable routes */}
             <div className="hidden md:flex w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
+
+            {/*Content of the dashboard routes */}
             <main className="pt-[80px] md:pl-56 h-full">
                 {children}
             </main>
