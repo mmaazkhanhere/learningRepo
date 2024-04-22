b1b3b20aad6c2acfb3a2117ea19131e0f7a17e3b
Maaz Khan <mmaazkhan2107@gmail.com>
2024-04-22 22:07:13 +0500

    courses data table completed

--- lms-tutorial/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ lms-tutorial/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,22 +1,36 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+
 import React from 'react'
+import { DataTable } from './_components/DataTable'
+import { columns } from './_components/Columns'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
 
 type Props = {}
 
-const Courses = (props: Props) => {
+
+const Courses = async (props: Props) => {
+
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect('/')
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId: userId,
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
     return (
         <div
             className='p-6'
         >
-            <Link
-                href='/teacher/create'
-            >
-                <Button>
-                    New Course
-                </Button>
-            </Link>
-
+            <DataTable columns={columns} data={courses} />
         </div>
     )
 }
