81da57cc7b31c47b43c9a2e58af7fddb49aa2a77
Maaz Khan <mmaazkhan2107@gmail.com>
2024-04-23 16:20:41 +0500

    categories query

--- lms-tutorial/app/(dashboard)/(routes)/search/page.tsx
+++ lms-tutorial/app/(dashboard)/(routes)/search/page.tsx
@@ -1,10 +1,25 @@
+import { db } from '@/lib/db'
 import React from 'react'
+import Categories from './_components/Categories';
 
 type Props = {}
 
-const SearchPage = (props: Props) => {
+const SearchPage = async (props: Props) => {
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc"
+        }
+    });
+
     return (
-        <div>SearchPage</div>
+        <div
+            className='p-6'
+        >
+            <Categories
+                items={categories}
+            />
+        </div>
     )
 }
 
