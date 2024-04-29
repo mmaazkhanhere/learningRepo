9e70d06ee3a5de791d34c7f6e5a822869536ea2e
Maaz Khan <mmaazkhan2107@gmail.com>
2024-04-29 17:40:10 +0500

    course creation api completed

--- lms-tutorial/app/api/courses/route.ts
+++ lms-tutorial/app/api/courses/route.ts
@@ -1,3 +1,5 @@
+/*An api route that handles POST requests to create a new course */
+
 import { db } from "@/lib/db";
 import { isTeacher } from "@/lib/teacher";
 import { auth } from "@clerk/nextjs";
@@ -6,10 +8,16 @@ import { NextResponse } from "next/server"
 export async function POST(req: Request) {
     try {
 
-        const { userId } = auth();
-        const { title } = await req.json();
+        const { userId } = auth(); /*retrieves the user id from the auth function
+        provided by Clerk */
+
+        const { title } = await req.json(); //extract the title from the request
 
         if (!userId || !isTeacher(userId)) {
+
+            /*If there is no userId or if the current user is not a teacher, an
+            unauthorized message will be returned */
+
             return new NextResponse("Unauthorized", { status: 401 });
         }
 
@@ -18,9 +26,10 @@ export async function POST(req: Request) {
                 userId,
                 title
             }
-        })
+        }) /*Uses the prisma query to create a course and passes the user id
+        and title extracted from the request body */
 
-        return NextResponse.json(course);
+        return NextResponse.json(course); //return a json response containing the newly created course
 
     } catch (error) {
         console.log('[COURSES]', error)
