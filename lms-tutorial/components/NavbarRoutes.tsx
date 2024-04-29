eb21c0dee6baae321153d333a242426825be1553
Maaz Khan <mmaazkhan2107@gmail.com>
2024-04-29 15:55:14 +0500

    dynamic layout documentation completed

--- lms-tutorial/components/NavbarRoutes.tsx
+++ lms-tutorial/components/NavbarRoutes.tsx
@@ -1,40 +1,55 @@
+/*Global component that handles the rendering of navigation elements within a 
+navbar based on the current page and user authentication status */
+
 "use client"
 
 import { UserButton, useAuth } from '@clerk/nextjs'
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import { Button } from './ui/button'
-import { LogOut } from 'lucide-react'
 import Link from 'next/link'
+
+import { Button } from './ui/button'
 import SearchInput from './SearchInput'
+
 import { isTeacher } from '@/lib/teacher'
+
+import { LogOut } from 'lucide-react'
 
 type Props = {}
 
 const NavbarRoutes = (props: Props) => {
 
-    const pathname = usePathname();
+    const pathname = usePathname(); /*uses the hook to get the curren pathname 
+    of the url */
 
-    const isTeacherPage = pathname?.startsWith('/teacher');
-    const isCoursePage = pathname?.includes('/courses');
+    const isTeacherPage = pathname?.startsWith('/teacher'); /*check if the current
+    user is on teacher page by checking the string in the url pathname.*/
 
-    const isSearchPage = pathname === "/search"
+    const isCoursePage = pathname?.includes('/courses'); /*check if the current
+    user is on cource page by checking the string in the url pathname */
 
-    const { userId } = useAuth();
+    const isSearchPage = pathname === "/search" /*user is on search page if the
+    url pathname is similar to /search */
+
+    const { userId } = useAuth(); //uses the clerk hook to get the current user id
 
 
     return (
         <>
             {
+                /*Search input component is rendered if the user is on the search
+                page which is hidden on small screen but visible on large screens*/
                 isSearchPage && (
                     <div className='hidden md:block'>
                         <SearchInput
-
                         />
                     </div>
                 )
             }
             <div className='flex gap-x-2 ml-auto'>
+
+                {/*Displayed if the user is on teacher page or on the course
+                page.It displays an exit button to exit from teacher mode */}
                 {
                     isTeacherPage || isCoursePage ? (
                         <Link
@@ -51,6 +66,8 @@ const NavbarRoutes = (props: Props) => {
                             </Button>
                         </Link>
                     ) : isTeacher(userId) ? (
+                        /*If the current is user is teacher, a teacher mode button
+                        is displayed otherwise nothing is displayed */
                         <Link
                             href='/teacher/courses'
                         >
@@ -63,9 +80,12 @@ const NavbarRoutes = (props: Props) => {
                         </Link>
                     ) : null
                 }
+
+                {/*A sign out button */}
                 <UserButton
                     afterSignOutUrl='/sign-in'
                 />
+
             </div>
         </>
 
