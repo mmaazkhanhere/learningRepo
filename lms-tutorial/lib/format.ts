962a2f26749564d3ee7b37246337f68a9fb62400
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-06-27 14:52:47 +0500

    jest crash course

--- lms-tutorial/lib/format.ts
+++
@@ -1,8 +0,0 @@
-/*A utility function used to format a numerical price value into a currency format */
-
-export const formatPrice = (price: number) => {
-    return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(price);
-}
